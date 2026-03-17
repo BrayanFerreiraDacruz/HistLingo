5ba236a4fc952e64dfdc67d2d6f7a3e6dca4091e
BrayanFerreiraDacruz <190931200+BrayanFerreiraDacruz@users.noreply.github.com>
2026-03-17 07:33:07 -0400

    fix: adicionar FrontendController para servir index.html em rotas SPA

---
+++ src/main/java/com/histlimgo/api/v1/controller/FrontendController.java
@@ -0,0 +1,23 @@
+package com.histlimgo.api.v1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+
+    @GetMapping(value = "/{path:[^\\.]*}/{subpath:[^\\.]*}")
+    public String redirect2() {
+        return "forward:/index.html";
+    }
+}
